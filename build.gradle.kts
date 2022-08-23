1b11277c207d8b35b33454db0145e95f38d5f194
valb3r <valb3r.ua@gmail.com>
2022-08-23 07:24:15 +0300

    Use JDBC http session

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.session:spring-session-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
--- src/main/kotlin/com/valb3r/deeplearning4j_trainer/Deeplearning4j_TrainerSpringApp.kt
+++ src/main/kotlin/com/valb3r/deeplearning4j_trainer/Deeplearning4j_TrainerSpringApp.kt
@@ -6,7 +6,11 @@ import com.valb3r.deeplearning4j_trainer.config.UsersConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
+@EnableJdbcHttpSession
+@EnableTransactionManagement
 @EnableConfigurationProperties(DirectoriesConfig::class, UsersConfig::class, S3Config::class)
 @SpringBootApplication(scanBasePackageClasses = [Deeplearning4j_TrainerSpringApp::class])
 class Deeplearning4j_TrainerSpringApp {
