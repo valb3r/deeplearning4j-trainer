12fd0d34aee7ef43b824679b687bc1a4995369a2
valb3r <valb3r.ua@gmail.com>
2022-08-26 17:31:17 +0300

    Fairer schedule via queue

--- src/main/kotlin/com/valb3r/deeplearning4j_trainer/config/FlowableConfig.kt
+++ src/main/kotlin/com/valb3r/deeplearning4j_trainer/config/FlowableConfig.kt
@@ -9,12 +9,19 @@ import com.valb3r.deeplearning4j_trainer.config.serde.OldTrainingSerde
 import com.valb3r.deeplearning4j_trainer.config.serde.TrainingSerde
 import com.valb3r.deeplearning4j_trainer.config.serde.ValidationSerde
 import org.flowable.common.engine.api.async.AsyncTaskExecutor
+import org.flowable.common.engine.impl.AbstractEngineConfiguration
+import org.flowable.common.engine.impl.EngineConfigurator
+import org.flowable.common.engine.impl.Page
+import org.flowable.job.service.JobServiceConfiguration
+import org.flowable.job.service.impl.persistence.entity.JobEntity
+import org.flowable.job.service.impl.persistence.entity.data.impl.MybatisJobDataManager
 import org.flowable.spring.SpringProcessEngineConfiguration
 import org.flowable.spring.boot.EngineConfigurationConfigurer
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.AsyncListenableTaskExecutor
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.validation.annotation.Validated
 import java.util.concurrent.Callable
@@ -23,7 +30,7 @@ import javax.validation.constraints.Min
 
 
 @Configuration
-class FlowableConfig {
+class FlowableConfig{
     /**
      * Customizes flowable so that it can store custom classes (not ones that implement Serializable) as
      * JSON as variables in database.
@@ -55,42 +62,25 @@ class FlowableConfig {
     }
 
     @Bean
-    fun flowableTaskExecutor(poolCfg: FlowableExecutorPoolConfig): EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
-        val asyncExecutor = NoQueueTaskExecutor(poolCfg)
-        asyncExecutor.initialize()
-        return EngineConfigurationConfigurer { processConfiguration: SpringProcessEngineConfiguration ->
-            processConfiguration.asyncTaskExecutor = asyncExecutor
-        }
+    fun taskExecutor(cfg: FlowableExecutorPoolConfig): AsyncListenableTaskExecutor {
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = cfg.corePoolSize
+        executor.maxPoolSize = cfg.maxPoolSize
+        executor.queueCapacity = cfg.queueCapacity
+        executor.threadNamePrefix = "flowable-executor"
+        executor.setAwaitTerminationSeconds(30)
+        executor.setWaitForTasksToCompleteOnShutdown(true)
+        executor.initialize()
+        return executor
     }
 
-    class NoQueueTaskExecutor(poolCfg: FlowableExecutorPoolConfig): ThreadPoolTaskExecutor(), AsyncTaskExecutor {
-        init {
-            this.corePoolSize = poolCfg.corePoolSize
-            this.maxPoolSize = poolCfg.maxPoolSize
-            this.queueCapacity = 1
-            this.threadNamePrefix = "flowable-executor"
-            this.setAwaitTerminationSeconds(30)
-            this.setWaitForTasksToCompleteOnShutdown(true)
-        }
-
-        override fun submit(task: Runnable): CompletableFuture<*> {
-            return super.submitListenable(task).completable()
-        }
-
-        override fun <T> submit(task: Callable<T>): CompletableFuture<T> {
-            return super.submitListenable(task).completable()
-        }
-
-        override fun getRemainingCapacity(): Int {
-            return maxPoolSize - activeCount
-        }
-    }
 
     @Validated
     @ConstructorBinding
     @ConfigurationProperties(prefix = "flowable-executor.pool")
     data class FlowableExecutorPoolConfig(
         @Min(1) val corePoolSize: Int,
-        @Min(1) val maxPoolSize: Int
+        @Min(1) val maxPoolSize: Int,
+        @Min(1) val queueCapacity: Int
     )
 }
